55647684232eeebe795c96537e752b8dc328cc3b
Alan Hong <xiupan@gmail.com>
2017-11-22 13:08:34 -0600

    Fixed it! pdfmake now works with React! Hooray

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-// import pdfMake from './pdfmake.min.js';
+import pdfMake from "pdfmake/build/pdfmake";
+import pdfFonts from "pdfmake/build/vfs_fonts";
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
 
 let docDefinition = { content: 'This is an sample PDF printed with pdfMake' };
 
 class App extends Component {
   render() {
+    // Opens the generated PDF in a new window on App load!
     // pdfMake.createPdf(docDefinition).open();
+
+    // Downloads the generated PDF as a .pdf file on App load!
+    // pdfMake.createPdf(docDefinition).download();
     return (
       <div className="App">
         <header className="App-header">
