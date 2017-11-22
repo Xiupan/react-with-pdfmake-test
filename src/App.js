ff5386773c8b8c9139c8d77731f1a754df5dae58
Alan Hong <xiupan@gmail.com>
2017-11-22 12:38:34 -0600

    Yeah... that no workies.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+// import pdfMake from './pdfmake.min.js';
+
+let docDefinition = { content: 'This is an sample PDF printed with pdfMake' };
 
 class App extends Component {
   render() {
+    // pdfMake.createPdf(docDefinition).open();
     return (
       <div className="App">
         <header className="App-header">
